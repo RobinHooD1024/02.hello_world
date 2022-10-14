b6fec8795ac91b0ff5fbaf03da4b4ad0c44be989
Andrey Teterin <robinhood.ex@gmail.com>
2022-10-14 12:01:41 -0400

    Init commit

---
+++ move_me_out.cpp
@@ -0,0 +1,9 @@
+//
+// Created by robin on 10/14/2022.
+//
+
+#include "move_me_out.h"
+
+void todo_move_me_out() {
+    std::cout << "This function should be located in the separated source file" << std::endl;
+}
